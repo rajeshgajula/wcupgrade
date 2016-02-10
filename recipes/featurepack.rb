4229c4d06d6ea0a8f5b1c559d62af27b20256d02
rajeshgajula <rg124@2098aadm114425l.tmw.com>
2016-02-09 16:36:21 -0800

    cookbook and skeleton recipes creation
    cookbook and skeleton recipes creation

---
+++ recipes/featurepack.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: wcupgrade
+# Recipe:: featurepack
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
---
+++ recipes/updateinstaller.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: wcupgrade
+# Recipe:: updateinstaller
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
