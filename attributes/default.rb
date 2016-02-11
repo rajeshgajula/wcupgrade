75f9648f20371673449f40684f1bf04b8adb6e58
rajeshgajula <rg124@2098aadm114425l.tmw.com>
2016-02-11 11:58:57 -0800

    added attributes for env paths
    added attributes for env paths

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+default['wcupgrade']['user'] = 'wcadmin'
+default['wcupgrade']['group'] = 'wctest'
+
+default['wcupgrade']['wc_home'] = '/usr/opt/app/IBM/WebSphere/CommerceServer'
+default['wcupgrade']['was_home'] = '/usr/opt/app/IBM/WebSphere/AppServer'
+default['wcupgrade']['ihs_home'] = '/usr/opt/app/HTTPServer'
+default['wcupgrade']['was_updi'] = '/usr/opt/app/IBM/WebSphere/was_updi'
+default['wcupgrade']['wcs_updi'] = '/usr/opt/app/IBM/WebSphere/wcs_updi'
